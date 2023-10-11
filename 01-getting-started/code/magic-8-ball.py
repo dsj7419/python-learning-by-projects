dca2575a83970d37555d234455fcd434000f5391
dsj7419 <dsj7419@gmail.com>
2023-10-11 07:25:36 -0700

    added magic 8 ball skeleton

---
+++ 01-getting-started/code/magic-8-ball.py
@@ -0,0 +1,22 @@
+# Import necessary module
+# HINT: You'll need a module to make random choices.
+
+# Define a list of possible answers for the Magic 8-Ball
+# HINT: Create a list containing different string answers that the Magic 8-Ball might give.
+
+# Prompt the user to ask a question
+# HINT: Use a function that allows the program to take user input.
+
+# Validate the user's input: Ensure it ends with a question mark
+# HINT: Check the last character of the input string. If it's valid, proceed; otherwise, give a message.
+
+    # If the input is valid:
+        # Randomly select an answer
+        # HINT: Use a function from the imported module to select a random item from your answers list.
+        
+        # Display the answer
+        # HINT: Use a function that outputs text to the console. Make sure to concatenate the chosen answer.
+    
+    # If the input is invalid:
+        # Display an error message
+        # HINT: Use a function that outputs text to the console, guiding the user to input a valid question.
