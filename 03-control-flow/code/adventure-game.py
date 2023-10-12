a1f4ecfcacea01f64e6d832e92473d0b41d3bd9d
dsj7419 <dsj7419@gmail.com>
2023-10-12 06:47:59 -0700

    Added skeleton code for lesson 3

---
+++ 03-control-flow/code/adventure-game.py
@@ -0,0 +1,24 @@
+# Adventure Game Skeleton
+
+# HINT: Display a welcome message and a brief description of the game scenario using print statements.
+# YOUR CODE HERE
+
+# Main game loop
+# HINT: Use a loop to allow the game to continue until the user decides to exit.
+# YOUR CODE HERE
+
+    # Choose a door
+    # HINT: Use a loop to ensure the user provides a valid input (1 or 2).
+    # YOUR CODE HERE
+    
+    # Determine the outcome
+    # HINT: Use conditionals to check the user's choice and display an appropriate message.
+    # YOUR CODE HERE
+    
+    # Play again?
+    # HINT: Ask the user if they want to play again and use a loop to validate their input.
+    # YOUR CODE HERE
+    
+    # Exit if the user doesn't want to play again
+    # HINT: Use a conditional to check the user's choice and exit the game if they don't want to play again.
+    # YOUR CODE HERE
