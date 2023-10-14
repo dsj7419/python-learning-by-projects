1cc6be03d696dcb51b09145936363748a44dc6ef
dsj7419 <dsj7419@gmail.com>
2023-10-14 13:16:37 -0700

    large revert from aria code. will do it later

--- 01-getting-started/quiz/script.js
+++ 01-getting-started/quiz/script.js
@@ -1,155 +1,172 @@
-// Utilize 'const' and 'let' for variable declarations.
-const getLetterGrade = (percentage) => {
-    // Simplify the if-else chain using ternary operators.
-    return percentage >= 90 ? 'A' : 
-           percentage >= 80 ? 'B' : 
-           percentage >= 70 ? 'C' : 
-           percentage >= 60 ? 'D' : 'F';
+function getLetterGrade(percentage) {
+    if (percentage >= 90) return 'A';
+    else if (percentage >= 80) return 'B';
+    else if (percentage >= 70) return 'C';
+    else if (percentage >= 60) return 'D';
+    else return 'F';
 }
-
-// Extract HTML elements once and use them throughout the script.
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
 const nextButtonElement = document.getElementById('next-button');
 const resultContainerElement = document.getElementById('result-container');
 const finalScoreElement = document.getElementById('final-score');
-const progressBarContainer = document.getElementById('progress-bar-container');
-const progressBar = document.getElementById('progress-bar');
-const answerFeedback = document.getElementById('answer-feedback');
 
-let shuffledQuestions, currentQuestionIndex, score;
+let shuffledQuestions, currentQuestionIndex;
+let score = 0;
 
-// Utilize async/await syntax for asynchronous operations.
-const startQuiz = async () => {
-    try {
-        const response = await fetch('questions.json');
-        if (!response.ok) throw new Error('Network response was not ok' + response.statusText);
-        const questions = await response.json();
-
-        score = 0;
-        shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-        currentQuestionIndex = 0;
-        questionContainerElement.classList.remove('hide');
-        progressBarContainer.classList.remove('hide');
-        setNextQuestion();
-    } catch (error) {
+fetch('questions.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(questions => {
+        startQuiz(questions);
+    })
+    .catch(error => {
         console.error('There has been a problem with your fetch operation:', error);
-    }
+    });
+
+function startQuiz(questions) {
+    score = 0;
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    document.getElementById('progress-bar-container').classList.remove('hide');
+    setNextQuestion();
 }
 
-const setNextQuestion = () => {
+function setNextQuestion() {
     document.getElementById('question-number').innerText = `Question ${currentQuestionIndex + 1} of ${shuffledQuestions.length}`;
-    answerFeedback.classList.add('hide');
+    document.getElementById('answer-feedback').classList.add('hide');
     resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
     updateProgressBar();
-    document.getElementById('restart').style.display = 'none';
-    document.getElementById('back-to-lesson').style.display = 'none';
 }
 
-const resetState = () => {
+function resetState() {
     nextButtonElement.classList.add('hide');
-    Array.from(answerButtonsElement.children).forEach(button => {
-        button.disabled = false;
-        button.remove();
-    });
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.firstChild.disabled = false; // Re-enable the button
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
 }
 
-const updateProgressBar = () => {
+function updateProgressBar() {
+    const progressBar = document.getElementById('progress-bar');
     const progressPercentage = ((currentQuestionIndex + 1) / shuffledQuestions.length) * 100;
+    console.log('Updating progress bar:', progressPercentage);
     progressBar.style.width = progressPercentage + '%';
 }
 
-const showQuestion = (question) => {
-    // Utilize template literals for string concatenation.
-    questionElement.innerHTML = question.code ? 
-        `${question.question}<br><br><pre><code>${question.code}</code></pre>` : 
-        question.question;
+function showQuestion(question) {
+    if (question.code) {
+        questionElement.innerHTML = question.question + "<br><br><pre><code>" + question.code + "</code></pre>";
+    } else {
+        questionElement.innerHTML = question.question;
+    }
     
     const shuffledAnswers = question.answers.sort(() => Math.random() - 0.5);
+    
     const labels = ['a)', 'b)', 'c)', 'd)'];
     
     shuffledAnswers.forEach((answer, index) => {
         const button = document.createElement('button');
-        // Use destructuring for better readability.
-        const {code, text, correct} = answer;
-        button.innerHTML = `${labels[index]} ${code ? `<pre><code>${code}</code></pre>` : text}`;
+        
+        let answerText = labels[index] + ' ';
+        if (answer.code) {
+            answerText += '<pre><code>' + answer.code + '</code></pre>';
+        } else {
+            answerText += answer.text;
+        }
+        
+        button.innerHTML = answerText;
         button.classList.add('btn');
-        // Utilize dataset properties directly instead of conditionally.
-        button.dataset.correct = correct;
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
         button.addEventListener('click', selectAnswer);
         answerButtonsElement.appendChild(button);
     });
 }
 
-const setStatusClass = (element, correct) => {
-    // Simplify class manipulation by using classList methods and conditional (ternary) operator.
-    element.classList.add(correct ? 'correct' : 'wrong');
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
 }
 
-const clearStatusClass = (element) => {
-    element.classList.remove('correct', 'wrong');
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
 
-const selectAnswer = (e) => {
+function resetState() {
+    nextButtonElement.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+}
+
+function selectAnswer(e) {
     const selectedButton = e.target;
-    const correct = selectedButton.dataset.correct === "true"; // Ensure correct is a boolean
+    const correct = selectedButton.dataset.correct;
     const currentQuestion = shuffledQuestions[currentQuestionIndex];
 
-    answerFeedback.classList.remove('hide');
+    document.getElementById('answer-feedback').classList.remove('hide');
 
     if (correct) {
         score++;
-        answerFeedback.innerHTML = `<span style="color: green;">&#10004; Correct!</span> ${currentQuestion.reason}`;
+        document.getElementById('answer-feedback').innerHTML = `<span style="color: green;">&#10004; Correct!</span> ${currentQuestion.reason}`;
         setStatusClass(selectedButton, true);
     } else {
-        answerFeedback.innerHTML = `<span style="color: red;">&#10008; Incorrect:</span> ${currentQuestion.reason}`;
+        document.getElementById('answer-feedback').innerHTML = `<span style="color: red;">&#10008; Incorrect:</span> ${currentQuestion.reason}`;
         setStatusClass(selectedButton, false);
         
         // Highlight the correct answer in green
         Array.from(answerButtonsElement.children).forEach(button => {
-            if (button.dataset.correct === "true") setStatusClass(button, true); // Ensure correct comparison
+            if (button.dataset.correct) setStatusClass(button, true);
         });
     }
 
-    Array.from(answerButtonsElement.children).forEach(button => button.disabled = true);
+    Array.from(answerButtonsElement.children).forEach(button => {
+        button.disabled = true;
+    });
 
-    if (currentQuestionIndex === shuffledQuestions.length - 1) {
-        nextButtonElement.innerText = 'Finish';
-        nextButtonElement.removeEventListener('click', goToNextQuestion);
-        nextButtonElement.addEventListener('click', finishQuiz);
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButtonElement.classList.remove('hide');
     } else {
-        nextButtonElement.innerText = 'Next';
-        nextButtonElement.removeEventListener('click', finishQuiz);
-        nextButtonElement.addEventListener('click', goToNextQuestion);
+        nextButtonElement.innerText = 'Finish';  // Change button text to 'Finish'
+        nextButtonElement.classList.remove('hide');
+        nextButtonElement.addEventListener('click', finishQuiz);  // Add event listener to finish the quiz
     }
-    nextButtonElement.classList.remove('hide');
 }
 
-const goToNextQuestion = () => {
-    currentQuestionIndex++;
-    setNextQuestion();
-}
-
-const finishQuiz = () => {
+function finishQuiz() {
     showScore();
     nextButtonElement.innerText = 'Next'; 
-    nextButtonElement.removeEventListener('click', finishQuiz);
+    nextButtonElement.removeEventListener('click', finishQuiz);  
 }
 
-const showScore = () => {
+function showScore() {
     questionContainerElement.classList.add('hide');
     resultContainerElement.classList.remove('hide');
-    const percentage = (score / shuffledQuestions.length) * 100;
-    const grade = getLetterGrade(percentage);
-    progressBarContainer.classList.add('hide');
-    finalScoreElement.textContent = `Your score: ${score}/${shuffledQuestions.length} (${percentage}% - Grade: ${grade})`;
-    document.getElementById('restart').style.display = 'inline-block';
-    document.getElementById('back-to-lesson').style.display = 'inline-block';
+    let percentage = (score / shuffledQuestions.length) * 100;
+    let grade = getLetterGrade(percentage);
+    document.getElementById('progress-bar-container').classList.add('hide');
+    finalScoreElement.textContent = `Your score: ${score}/${shuffledQuestions.length} (${percentage}% - Grade: ${grade})`;  
 }
 
-// Event listener for the 'Next' button.
+nextButtonElement.addEventListener('click', () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+
 document.getElementById('back-to-lesson').addEventListener('click', () => {
     window.location.href = 'https://github.com/dsj7419/python-learning-by-projects/blob/main/01-getting-started/README.md#quiz';
 });
@@ -159,8 +176,5 @@ document.getElementById('restart').addEventListener('click', () => {
     score = 0;
     resultContainerElement.classList.add('hide');
     questionContainerElement.classList.remove('hide');
-    startQuiz();
+    startQuiz(shuffledQuestions);
 });
-
-// Start the quiz when the script loads.
-startQuiz();
